5ec0cb9677ae3daf8ca4b146af295647719ed9f9
Berdikulov-571 <mygithub571@gmail.com>
2023-10-23 15:53:07 +0500

    Completed

--- ForHomeWorks/Program.cs
+++ ForHomeWorks/Program.cs
@@ -6,7 +6,7 @@ namespace ForHomeWorks
     {
         static void Main(string[] args)
         {
-            AllMethods.Task21(new List<char> { '1','2','3','4','5','6','7'});
+
         }
     }
 }
