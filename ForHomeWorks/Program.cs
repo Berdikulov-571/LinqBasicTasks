504c6f34cf9b79999d0e4a3e90c8633135188419
Berdikulov-571 <mygithub571@gmail.com>
2023-10-23 18:17:52 +0500

    Madur bo'ldi

--- ForHomeWorks/Program.cs
+++ ForHomeWorks/Program.cs
@@ -1,12 +1,44 @@
-﻿using System.Collections;
-
-namespace ForHomeWorks
+﻿namespace ForHomeWorks
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            List<Student> students = new List<Student>()
+            {
+                new Student() {Id = 1,Name = "Bob"},
+                new Student() {Id = 2,Name = "Eshmat"},
+                new Student() {Id = 3,Name = "Toshmat"},
+                new Student() {Id = 4,Name = "Lochin"},
+                new Student() {Id = 5,Name = "Toshmat"},
+                new Student() {Id = 6,Name = "Toshtemir"},
+                new Student() {Id = 7,Name = "G'ishmat"},
+                new Student() {Id = 8,Name = "Yalqov"}
+            };
 
+            List<Student> students2 = new List<Student>()
+            {
+                new Student() {Id = 1,Name = "Bob"},
+                new Student() {Id = 2,Name = "Eshmat"},
+                new Student() {Id = 3,Name = "Toshmat"},
+                new Student() {Id = 4,Name = "Lochin"},
+                new Student() {Id = 5,Name = "Toshmat"},
+                new Student() {Id = 6,Name = "Toshtemir"},
+                new Student() {Id = 7,Name = "G'ishmat"},
+                new Student() {Id = 8,Name = "Yalqov"}
+            };
+
+            var result = students.Except(students2,new StudentComparer()).ToList();
+
+            foreach (var i in result)
+            {
+                Console.WriteLine(i.Name);
+            }
         }
+    }
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
---
+++ ForHomeWorks/StudentComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ForHomeWorks
+{
+    internal class StudentComparer : IEqualityComparer<Student>
+    {
+        public bool Equals(Student? x, Student? y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetHashCode([DisallowNull] Student obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
